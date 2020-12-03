06c236442b4cb46d5c3132dc7b470fdc5608dfe5
beryll1um <vitalii.babichuk@gmail.com>
2020-12-03 20:11:21 +0200

    fix('QMLTemplate/src/main.cpp'): unnecessary includes were excluded

--- QMLTemplate/src/main.cpp
+++ QMLTemplate/src/main.cpp
@@ -1,10 +1,5 @@
 #include "core/launcher/launcher.hpp"
 
-#include <QApplication>
-#include <QQmlApplicationEngine>
-
-#include "application/ui/manager.hpp"
-
 int main(int argc, char *argv[]) {
     const auto launcher = Core::Launcher::Create(argc, argv);
     return launcher ? launcher->exec() : 1;
